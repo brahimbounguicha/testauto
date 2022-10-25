8332dbe0f4e3b3e8cac0bc53064c4e320d1f95c0
Brahim BOUNGUICHA <bbounguicha@dom.tti>
2022-10-25 12:27:53 +0200

    my change

---
+++ test-output-thread/report.js
@@ -0,0 +1,81 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "bd00556f-07ca-41f7-ae08-bc3f99b783c7",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts section count",
+    "start": 1666693570592,
+    "group": 20,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1666693586884,
+    "className": "failed"
+  },
+  {
+    "id": "7687aa07-99b2-4720-b646-727162f3624f",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1666693570591,
+    "group": 23,
+    "content": "",
+    "tags": "",
+    "end": 1666693588131,
+    "className": "failed"
+  },
+  {
+    "id": "59475c59-3a87-4ca2-8ec5-70cef9f5e37b",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1666693570542,
+    "group": 22,
+    "content": "",
+    "tags": "",
+    "end": 1666693587210,
+    "className": "failed"
+  },
+  {
+    "id": "8bac825c-21e9-465b-9940-e81576aeea53",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1666693570592,
+    "group": 21,
+    "content": "",
+    "tags": "",
+    "end": 1666693587619,
+    "className": "failed"
+  },
+  {
+    "id": "1120e7ec-a678-4be7-8271-58873303117c",
+    "feature": "Account Page Feature",
+    "scenario": "Accounts page title",
+    "start": 1666693570600,
+    "group": 19,
+    "content": "",
+    "tags": "@accounts,",
+    "end": 1666693586146,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 19,
+    "content": "Thread[TestNG-PoolService-0,5,main]"
+  },
+  {
+    "id": 20,
+    "content": "Thread[TestNG-PoolService-1,5,main]"
+  },
+  {
+    "id": 21,
+    "content": "Thread[TestNG-PoolService-2,5,main]"
+  },
+  {
+    "id": 22,
+    "content": "Thread[TestNG-PoolService-3,5,main]"
+  },
+  {
+    "id": 23,
+    "content": "Thread[TestNG-PoolService-4,5,main]"
+  }
+]);
+});
